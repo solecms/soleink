5425c4c139ac52f91dd9a7cf5f8f88955d49a373
Taz Sen <taz-sen@outlook.com>
2024-07-26 21:04:54 +1200

    :recycle: ➕ Add sync-pr-issue workflow #42
    	new file:   .github/scripts/get-issue.sh
    	new file:   .github/workflows/sync-pr-issue.yml

---
+++ .github/scripts/get-issue.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+SECRET=$1
+OWNER=$2
+REPO=$3
+ISSUE_NUMBER=$4
+
+if [ -z "$SECRET" ] || [ -z "$OWNER" ] || [ -z "$REPO" ] || [ -z "$ISSUE_NUMBER" ]; then
+    echo "Usage: $0 <SECRET> <OWNER> <REPO> <ISSUE_NUMBER>"
+    exit 1
+fi
+
+issue=$(curl -L \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer $SECRET" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    https://api.github.com/repos/$OWNER/$REPO/issues/$ISSUE_NUMBER)
+
+echo "ISSUE=$(echo "$issue" | jq -c .)" >> $GITHUB_ENV
